2f8530d5a9f22b1539d47c216e81b3544bee6427
Francescu Garoby <francescu.garoby@docndoc.fr>
2021-12-02 14:41:33 +0100

    [ADD]Vuejs stuff

---
+++ resources/js/components/MyMap.vue
@@ -0,0 +1,54 @@
+<template>
+    <l-map
+        id="myMap"
+        ref="myMap"
+        style="height: 100%; width: 100%"
+        :zoom="zoom"
+        :center="center"
+        @ready="loadMarkers()">
+        <l-tile-layer
+            :url="url"
+            :attribution="attribution"></l-tile-layer>
+        <l-marker v-for="marker in markers" :key="marker.id" :latLng="marker.latLng">
+        </l-marker>
+    </l-map>
+</template>
+
+<script>
+// If you need to reference 'L', such as in 'L.icon', then be sure to
+// explicitly import 'leaflet' into your component
+import Axios from 'axios';
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+
+export default {
+  name: 'MyMap',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  data() {
+      return {
+          zoom: 13,
+          center: latLng(49.18222, -0.37056),
+          url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+          markers: [],
+      }
+  },
+  methods: {
+      loadMarkers() {
+          Axios.get('/api/markers')
+               .then((response) => {
+                   response.data.data.forEach(marker => {
+                       this.markers.push({
+                           id: marker.id,
+                           latLng: [marker.latitude, marker.longitude],
+                       });
+                   });
+               });
+      }
+  }
+};
+</script>
